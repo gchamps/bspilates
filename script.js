1cac3074663da5ccf767c6644a07d6e9fb3199df
gchamps <gabriel.champin97@gmail.com>
2025-06-22 17:43:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Scroll suave para os links do menu
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
